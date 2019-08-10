71dd38dc0ed38a05488e6bbc9a0f0bd3bade2757
Andrés <lag1792@live.com>
2019-08-10 01:38:17 -0500

    Commit regresando a una version funcional del proyecto, aunque CI no lo recomiende

--- fakestagram/AppDelegate.swift
+++ fakestagram/AppDelegate.swift
@@ -6,8 +6,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        StorageType.permanent.ensureExists()
+        StorageType.cache.ensureExists()
+        
         AccountRepo.shared.loadOrCreate { (account) in
-            print(account)
+            if let uuid = account.id{
+                _ = Secrets.token.set(value: uuid)
+            }
         }
         return true
     }
